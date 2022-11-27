c96e918348050d0731a9d51369e335f801796947
Rahul yadav <beingcooder@gmail.com>
2022-11-27 21:24:12 +0530

    vercel added

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,7 @@ import { userRouter } from './routes';
 const app = express();
 app.use(express.json());
 app.use('/api/v1/user', userRouter)
-app.use('/', (req, res) => {
-  res.status(200).json({
-    message: 'You are conencted',
-    success: true
-  })
-})
+
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 });
