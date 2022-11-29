b6f350416b6c90a34e51d00d4bec7042e2c37b68
Rahul yadav <beingcooder@gmail.com>
2022-11-29 07:21:01 +0530

    test

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import app from './app';
+import app from './index';
 
 const PORT = process.env.PORT || 8000;
 const mongoConnection: string = `mongodb+srv://rahul_yadv:qmKQ8TDnHLmqmbLZ@cluster0.64cxwhe.mongodb.net`;
